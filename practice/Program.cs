77458d29093483e5ec3f35e60b34cc27a315f4fb
shaheerqureshi <shaheerqureshi334@gmail.com>
2023-11-07 07:16:47 -0500

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,62 @@
+﻿//WHILE FIGHTING
+
+//string name = "Shaheer Arshad";
+
+//Console.WriteLine($"Hello \n{name}");
+
+//------------------------------------------------------------------------------------
+//int num1 = 5;
+//int num2 = 6;
+//int temp = 0;
+
+//temp = num1;
+//num1 = num2;
+//num2 = temp;
+
+//Console.WriteLine($"Number 1 is {num1} and num2 is {num2}");
+//------------------------------------------------------------------------------------
+//Console.WriteLine("Lets Enter some Numbers");
+
+//Console.WriteLine("Please Enter First Number");
+//int num1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Please Enter Second Number");
+//int num2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Please Enter Third Number");
+//int num3 = int.Parse(Console.ReadLine());
+
+//int sum = num1 * num2 * num3;
+//Console.WriteLine($"{num1} X {num2} X {num3} = {sum}");
+
+//------------------------------------------------------------------------------------
+
+//Console.WriteLine("Enter a number to see the table of");
+//int num = int.Parse(Console.ReadLine());
+
+//for (int i = 1; i <=10 ; i++)
+//{
+//    Console.WriteLine($"{num} x {i} = {num*i}");
+//}
+//------------------------------------------------------------------------------------
+//AVERAGE
+
+//List<int> marks = new List<int> ();
+//int subjects = 4;
+//float totalAverage;
+//int sum = 0;
+//int mark = 0;
+//for (int i = 1; i<=subjects; i++)
+//{
+//    Console.WriteLine($"Enter the Marks for Subject {i}");
+//    mark =  int.Parse(Console.ReadLine());
+//    marks.Add(mark);
+//}
+//Console.WriteLine("You have Entered");
+//foreach (var item in marks)
+//{
+//    sum += item;
+//}
+//totalAverage = sum / subjects;
+//Console.WriteLine(totalAverage);
+
+
+
